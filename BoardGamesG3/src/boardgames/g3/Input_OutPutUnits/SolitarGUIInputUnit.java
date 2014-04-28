9b6ae25399c9cd7dc999c55bd7be0af89820da40
AndersBlommaN <anders_blomkvist_89@live.se>
2014-04-28 11:45:28 +0200

    inputUnit är klar för Solitär

--- BoardGamesG3/src/boardgames/g3/Input_OutPutUnits/SolitarGUIInputUnit.java
+++ BoardGamesG3/src/boardgames/g3/Input_OutPutUnits/SolitarGUIInputUnit.java
@@ -15,49 +15,50 @@ import boardgames.g3.core.Solitaire.SolitarHelpMethods;
 
 public class SolitarGUIInputUnit extends InputUnit implements ActionListener {
 
- private GameState state;
+	private GameState state;
 
- BackGroundButtonID buttonGUI;
+	BackGroundButtonID buttonGUI;
 
- BoardLocation sourceClick = null;
- BoardLocation destinationClick = null;
+	BoardLocation sourceClick = null;
+	BoardLocation destinationClick = null;
 
- @Override
- public void setup(GameState gameState) {
-  this.state = gameState;
- }
+	@Override
+	public void setup(GameState gameState) {
+		this.state = gameState;
+	}
 
- public void onClick(String coordinate) {
+	public void onClick(String coordinate) {
 
+		if (sourceClick == null) {
+			sourceClick = SolitarHelpMethods.getBoardLocationFromCoordinate(
+					coordinate, state.getBoard());
+			if (sourceClick.getPiece() != null)
+				buttonGUI.setButtonMarked();
 
-  
-   if (sourceClick == null) {
-    buttonGUI.setButtonMarked();
-    sourceClick = SolitarHelpMethods.getBoardLocationFromCoordinate(coordinate,
-      state.getBoard());
+		} else if (sourceClick.getId().equals(coordinate)) {
+			if (sourceClick.getPiece() != null)
+				buttonGUI.setButtonWithBead();
 
-   } else if (sourceClick.getId().equals(coordinate) && coordinate == null) {
-    buttonGUI.setButtonWithBead();
-    sourceClick = null;
+			sourceClick = null;
 
-   } else if (sourceClick != null && destinationClick == null) {
-    destinationClick = SolitarHelpMethods.getBoardLocationFromCoordinate(
-      coordinate, state.getBoard());
+		} else if (sourceClick != null && destinationClick == null) {
+			destinationClick = SolitarHelpMethods
+					.getBoardLocationFromCoordinate(coordinate,
+							state.getBoard());
 
-    notifyListenersOfMove(new Move(state.getPlayerInTurn(), sourceClick,
-      destinationClick));
+			notifyListenersOfMove(new Move(state.getPlayerInTurn(),
+					sourceClick, destinationClick));
 
-    sourceClick = null;
-    destinationClick = null;
-   }
-  }
- 
+			sourceClick = null;
+			destinationClick = null;
+		}
+	}
 
- @Override
- public void actionPerformed(ActionEvent event) {
-  buttonGUI = (BackGroundButtonID) event.getSource();
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		buttonGUI = (BackGroundButtonID) event.getSource();
 
-  onClick(buttonGUI.getStringId());
+		onClick(buttonGUI.getStringId());
 
- }
+	}
 }
