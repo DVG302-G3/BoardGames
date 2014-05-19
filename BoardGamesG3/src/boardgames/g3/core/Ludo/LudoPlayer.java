cf19c9c83929212a67fd7e705122f35fc68299a3
Didriksson <mattias.didriksson@gmail.com>
2014-05-19 12:24:58 +0200

    Råkade förstöra lite. Fixade.

--- BoardGamesG3/src/boardgames/g3/core/Ludo/LudoPlayer.java
+++ BoardGamesG3/src/boardgames/g3/core/Ludo/LudoPlayer.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class LudoPlayer {
 
-	Player player;
-	List<String> home;
-	String startCoordinateOne;
-	String startCoordinateSix;
+	private Player player;
+	private List<String> home;
+	private List<String> finishLine;
+	private String startCoordinateOne;
+	private String startCoordinateSix;
 
 	public LudoPlayer(Player player) {
 		this.player = player;
@@ -37,26 +38,34 @@ public class LudoPlayer {
 		switch (player.getName()) {
 		case LudoStaticValues.GREENPLAYER:
 			home = LudoStaticValues.GREENHOME;
+			finishLine = LudoStaticValues.GREENFINISHLINE;
 			startCoordinateOne = LudoStaticValues.GREENSTART;
 			startCoordinateSix = LudoStaticValues.GREENSTARTSIXES;
 			break;
 		case LudoStaticValues.BLUEPLAYER:
 			home = LudoStaticValues.BLUEHOME;
+			finishLine = LudoStaticValues.BLUEFINISHLINE;
 			startCoordinateOne = LudoStaticValues.BLUESTART;
 			startCoordinateSix = LudoStaticValues.BLUESTARTSIXES;
 			break;
 		case LudoStaticValues.REDPLAYER:
 			home = LudoStaticValues.REDHOME;
+			finishLine = LudoStaticValues.REDFINISHLINE;
 			startCoordinateOne = LudoStaticValues.REDSTART;
 			startCoordinateSix = LudoStaticValues.REDSTARTSIXES;
 			break;
 		case LudoStaticValues.YELLOWPLAYER:
 			home = LudoStaticValues.YELLOWHOME;
+			finishLine = LudoStaticValues.YELLOWFINISHLINE;
 			startCoordinateOne = LudoStaticValues.YELLOWSTART;
 			startCoordinateSix = LudoStaticValues.YELLOWSTARTSIXES;
 
 		}
 
+	}
+
+	public List<String> getFinishLine() {
+		return finishLine;
 	}
 
 }
