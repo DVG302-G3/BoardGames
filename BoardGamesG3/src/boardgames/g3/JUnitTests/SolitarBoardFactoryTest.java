436ca01b105d37c4c95e41413d171d1a27b9befa
tyboni <ceciliatyboni@gmail.com>
2014-05-21 11:41:16 +0200

    testklasser

---
+++ BoardGamesG3/src/boardgames/g3/JUnitTests/SolitarBoardFactoryTest.java
@@ -0,0 +1,23 @@
+package boardgames.g3.JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import boardgames.g3.core.Solitaire.SolitarBoardFactory;
+
+public class SolitarBoardFactoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+		SolitarBoardFactory solitarBoardFactory = new SolitarBoardFactory();
+		
+	}
+
+	@Test
+	public void testPutAllTheBeadsOnTheBoard() {
+		fail("Not yet implemented");
+	}
+
+}
